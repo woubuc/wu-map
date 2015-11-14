22fc1fdb2ed3494ed3ebd897890ad789a58faf41
Wouter Buckens <wou.buc@gmail.com>
2015-11-14 03:16:46 +0100

    Added more locations

--- locations.js
+++ locations.js
@@ -5,13 +5,16 @@ locations = [
     name: 'New Town',
     tag: 'new-town',
     top: true,
+    mayor: 'Dominikk',
+    type: 'large',
     x: 907,
     y: 760,
-    features: ['starter']
+    features: ['trader', 'market', 'harbour', 'resources', 'recruiting']
   }, {
     name: 'Little Patch of Heaven',
     tag: 'little-patch-of-heaven',
     mayor: 'Jaede',
+    type: 'solo',
     x: 1115,
     y: 775
   }, {
@@ -32,8 +35,10 @@ locations = [
   }, {
     name: 'Tholen Farmstead',
     tag: 'tholen-farmstead',
+    mayor: 'Cunemous',
     x: 1025,
-    y: 770
+    y: 770,
+    features: ['resources', 'recruiting']
   }, {
     name: 'Zigsburg',
     tag: 'zigsburg',
@@ -49,5 +54,65 @@ locations = [
     tag: 'cedar-grove',
     x: 1586,
     y: 1027
+  }, {
+    name: 'Castrum Felis',
+    tag: 'castrum-felis',
+    type: 'solo',
+    mayor: 'Meow',
+    x: 738,
+    y: 1200,
+    features: ['recruiting']
+  }, {
+    name: 'The Druidic Conclave',
+    tag: 'the-druidic-conclave',
+    type: 'solo',
+    mayor: 'DruidNature',
+    x: 631,
+    y: 2108,
+    features: ['recruiting']
+  }, {
+    name: 'Symphonies End',
+    tag: 'symphonies-end',
+    type: 'small',
+    mayor: 'Melketh',
+    x: 1046,
+    y: 790,
+    features: ['recruiting']
+  }, {
+    name: 'Chris\' Home',
+    tag: 'chris-home',
+    type: 'solo',
+    mayor: 'Chris',
+    x: 730,
+    y: 2348
+  }, {
+    name: 'Goatsmugglers Place',
+    tag: 'goatsmugglers-place',
+    type: 'solo',
+    mayor: 'Goatsmuggler',
+    x: 822,
+    y: 1226,
+    features: ['harbour']
+  }, {
+    name: 'Highwater',
+    tag: 'highwater',
+    type: 'small',
+    mayor: 'Kezei',
+    x: 2628,
+    y: 346,
+    features: ['harbour', 'resources', 'recruiting']
+  }, {
+    name: 'Deathlands',
+    tag: 'deathlands',
+    type: 'solo',
+    mayor: 'Death',
+    x: 1059,
+    y: 704
+  }, {
+    name: 'Castleton',
+    tag: 'castleton',
+    owner: 'Rikkko',
+    x: 1217,
+    y: 1011
   }
 ];
